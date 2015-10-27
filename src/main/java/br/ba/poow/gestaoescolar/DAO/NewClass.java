49fee7b52773d5d7f25543ea3897d62812af7df8
Simone <Simone@Simone-PC>
2015-10-27 20:55:55 -0300

    teste do abusado sabino com eu

---
+++ src/main/java/br/ba/poow/gestaoescolar/DAO/NewClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ba.poow.gestaoescolar.DAO;
+
+/**
+ *
+ * @author Simone
+ */
+public class NewClass {
+    private int sabino;
+
+    public int getSabino() {
+        return sabino;
+    }
+
+    public void setSabino(int sabino) {
+        this.sabino = sabino;
+    }
+    
+    
+}
